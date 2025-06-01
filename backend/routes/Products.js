2640314f6bf9f858aaff8707da307861fedaa218
divy-11 <devvv.sangwan@gmail.com>
2025-06-01 23:54:22 +0530

    Integrated getProduct

--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -9,6 +9,38 @@ const formQuery = async (products, ingredients) => {
 
     return { products, nykaaRes };
 };
+// const vals = getProducts('minimal')
+// console.log(vals);
+const removeSame = (vals) => {
+    const ls = []
+    const { productAM, productPM } = vals;
+    const lenAM = productAM.length
+    const lenPM = productPM.length
+    let i = 0;
+    let j = 0;
+    while (i < lenAM && j < lenPM) {
+        if (productAM[i] == productPM[j]) {
+            ls.push(productAM[i])
+        } else {
+            ls.push(productAM[i])
+            ls.push(productPM[j])
+        }
+        i++;
+        j++;
+    }
+    while (i < lenAM) {
+        ls.push(productAM[i])
+        i++
+    }
+    while (j < lenPM) {
+        ls.push(productPM[j])
+        j++
+    }
+    return ls
+
+}
+// const lsFinal = removeSame(vals);
+// console.log(lsFinal);
 
 app.post("/", (req, res) => {
     const { commitment } = req.body;
@@ -16,8 +48,11 @@ app.post("/", (req, res) => {
 })
 
 app.get("/", async (req, res) => {
-    // const { steps, ingredients } = req.query;
-    const steps = ['Cleanser', 'Moisturizer']; // based on commitment
+    const { commitment } = req.body;
+    const vals = getProducts(commitment)
+    const steps = removeSame(vals);
+    console.log(steps);
+    // const steps = ['Cleanser', 'Moisturizer']; // based on commitment
     const ingredients = ['Niacinamide', 'Ceramides']; // from your API
 
     if (!steps || !ingredients) {
